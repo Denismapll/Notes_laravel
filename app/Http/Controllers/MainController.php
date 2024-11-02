d4785ce28c8b71db6f3a3e3d29ae9eab13467d2a
DENIS DE FERRAZ <denis.black65@gmail.com>
2024-11-02 03:17:52 -0300

    011

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,16 +2,126 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
+use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Http\Request;
+
 
 class MainController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        echo "I'm inside the app!";
+        // load user's notes
+        $id = session('user.id');
+        $notes = User::find($id)->notes()->get()->toArray();
+
+        return view('home', ['notes' => $notes]);
     }
     public function newNote()
     {
-        echo "Criando uma nota nova!";
+        // show new note view
+        return view('newNote');
     }
+
+    public function newNoteSubmit(Request $request){
+        
+        // validate request
+
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O title é obrigatório',
+                'text_title.min' => 'O titulo deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O titulo deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'o nota é obrigatório',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // get user id
+
+        $id = session('user.id');
+
+
+        // create new note
+
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        // redirect to home
+
+        return redirect()->route('home');
+    }
+
+    public function editNote($id)
+    {
+        // $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // show edit note view
+
+        return view('edit_note', ['note'=> $note]);
+
+    }
+
+    public function editNoteSubmit(Request $request) {
+
+        // validation
+
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000'
+            ],
+            [
+                'text_title.required' => 'O title é obrigatório',
+                'text_title.min' => 'O titulo deve ter pelo menos :min caracteres',
+                'text_title.max' => 'O titulo deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'o nota é obrigatório',
+                'text_note.min' => 'A nota deve ter pelo menos :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+        );
+
+        // check if note_id exists
+        if($request->note_id == null) {
+            return redirect()->route('home');
+        }
+
+        // decrypt note_id
+        $id = Operations::decryptId($request->note_id);
+
+        // load note
+        $note = Note::find($id);
+
+        // update note
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+        
+        // redirect to home
+        return redirect()->route('home');
+
+    }
+
+    public function deleteNote($id)
+    {
+        $id = Operations::decryptId($id);
+
+        
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,16 @@ Route::middleware([checkIsNotLogged::class])->group(function () {
 
 // App routes - user logged
 Route::middleware([checkIsLogged::class])->group(function () {
-    Route::get('/', [MainController::class, 'index']);
-    Route::get('/newNote', [MainController::class, 'newNote']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::get('/newNote', [MainController::class, 'newNote'])->name('newnote');
+    Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
+
+    // edit note
+    Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
+    Route::post('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
+    
+    // delete note
+    Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
